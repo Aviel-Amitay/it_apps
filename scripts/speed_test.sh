b86273dad281e2331ca3cf54990e8edb0277d0fc
Aviel Amitay <amitay.site@gmail.com>
2026-04-06 23:37:40 +0300

    Aviel Amitay
    Update running Speed Test with Python3.

--- scripts/speed_test.sh
+++ scripts/speed_test.sh
@@ -6,7 +6,7 @@
 # Modified: Jan 29 2025
 ################################################################################
 
-curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python - ; exit
+curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 - ; exit
 	;;
 	[a-z][A-Z]) echo "Please insert valid option"
 	;;
